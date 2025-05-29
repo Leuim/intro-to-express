e133f638c8bd6d5c2ec08bc48887f91ec5cef172
leuim <jassim.mohammed48@gmail.com>
2025-05-29 14:43:14 +0300

    Finish question 3

--- server.js
+++ server.js
@@ -18,6 +18,20 @@ app.get('/roll/:dice', (req, res) => {
     }
 })
 
+app.get('/collectibles/:collectIndex',(req,res)=>{
+      const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+  const index = Number(req.params.collectIndex)
+  if(index >= 0 && index < collectibles.length){
+    res.send(`So, you want the ${collectibles[index].name}? For ${collectibles[index].price}, it can be yours!`)
+  } else {
+    res.send('This item is not yet in stock. Check back soon!')
+  }
+})
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
